8f84e15e0d3bf7d0824c55219c46e48e635df2e4
Toofan <Toofan534@gmail.com>
2022-08-04 02:38:08 +0430

    eyeTracker system has been updated

--- resources/Gamemode/client/utils/eyeTrackerObjects.js
+++ resources/Gamemode/client/utils/eyeTrackerObjects.js
@@ -1,20 +1,20 @@
 export const eyeTrackerObjects = [
   {
     name: "atm",
+    titles: ["ATM"],
     HashIDs: [3424098598, 3168729781, 2930269768, 506770882],
   },
   {
     name: "Clothes_NPCs",
+    titles: ["Clothes", 1, 2],
     HashIDs: [920595805],
   },
   {
     name: "Gas_Pump",
+    titles: ["Gas Pump"],
     HashIDs: [
       1933174915, 2287735495, 1339433404, 1694452750, 3825272565, 3832150195,
       4130089803,
     ],
   },
 ];
-
-
-
