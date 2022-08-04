30a01cfaa7f03ba281d527ca7ee4190e74a865ef
Toofan <Toofan534@gmail.com>
2022-08-04 17:26:07 +0430

    CommandsTest.js (ServerSide) has been updated

--- resources/Gamemode/server/system/commandsTest.js
+++ resources/Gamemode/server/system/commandsTest.js
@@ -1,6 +1,7 @@
+/// <reference types="@altv/types-shared" />
 /// <reference types="@altv/types-server" />
 import * as alt from "alt-server";
-import { registerCmd } from "./chat";
+import { registerCmd, sendchat } from "./chat";
 
 registerCmd("g", (player) => {
   alt.emitClient(player, "teleportToWaypoint");
@@ -11,6 +12,22 @@ registerCmd("gp", (player) => {
       3
     )}, z: ${player.pos.z.toFixed(3)}`
   );
+});
+
+registerCmd("vs", (player, args) => {
+  if (!args) return;
+  if (!player.vehicle)
+    return sendchat(player, `{ff0000}You are not enter a vehicle`);
+  if (args[0] == "d") {
+    player.vehicle.driftModeEnabled
+      ? (player.vehicle.driftModeEnabled = false)
+      : (player.vehicle.driftModeEnabled = true);
+
+    sendchat(player, `Vehicle drift ${player.vehicle.driftModeEnabled}`);
+  }
+  if (args[0] == "r") {
+    player.vehicle.repair();
+  }
 });
 
 alt.on("playerDamage", (player) => {
