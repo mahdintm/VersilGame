c1d0ce754b8e418eccb14abbbdbddadbf8af5754
Toofan <Toofan534@gmail.com>
2022-08-04 16:20:16 +0430

    eyeTrackerObjects.js has been updated

--- resources/Gamemode/client/utils/eyeTrackerObjects.js
+++ resources/Gamemode/client/utils/eyeTrackerObjects.js
@@ -1,17 +1,22 @@
 export const eyeTrackerObjects = [
+  // action is not duplicated
   {
     name: "atm",
-    titles: ["ATM"],
+    titles: [{ title: "ATM", action: "" }],
     HashIDs: [3424098598, 3168729781, 2930269768, 506770882],
   },
   {
     name: "Clothes_NPCs",
-    titles: ["Clothes", 1, 2],
+    titles: [
+      { title: "Clothes", action: "OpenClothesUiBox" },
+      { title: "1", action: "1" },
+      { title: "2", action: "2" },
+    ],
     HashIDs: [920595805],
   },
   {
     name: "Gas_Pump",
-    titles: ["Gas Pump"],
+    titles: [{ title: "Gas Pump", action: "" }],
     HashIDs: [
       1933174915, 2287735495, 1339433404, 1694452750, 3825272565, 3832150195,
       4130089803,
