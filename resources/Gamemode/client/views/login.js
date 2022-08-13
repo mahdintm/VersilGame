221bf2bb9a3d3006b5e7121cba3216786047e6d2
Toofan <Toofan534@gmail.com>
2022-08-14 00:34:13 +0430

    keyUPController has been updated

--- resources/Gamemode/client/views/login.js
+++ resources/Gamemode/client/views/login.js
@@ -3,8 +3,88 @@ import * as native from "natives";
 import { webview_tools } from "./webviewscontroler";
 import { LocalStorage } from "../system/LocalStorage";
 import { View } from "./viewCreator";
+import { VGView } from "./webViewController";
+import { EventNames } from "../utils/eventNames";
+import { WebViewStatus } from "../utils/WebViewStatus";
 
 export async function LoadLoginPage() {
+  function LoginCamera() {
+    native.displayRadar(false);
+    const camera = native.createCam("DEFAULT_SCRIPTED_CAMERA", true);
+    native.setCamCoord(camera, 0, 0, 100);
+    native.setCamRot(camera, 0, 0, 0, 2);
+    native.setCamFov(camera, 90);
+    const camera2 = native.createCam("DEFAULT_SCRIPTED_CAMERA", true);
+    native.setCamCoord(camera2, 100, 1000, 1000);
+    native.setCamRot(camera2, 0, 0, 500, 2);
+    native.setCamFov(camera2, 90);
+    const camera3 = native.createCam("DEFAULT_SCRIPTED_CAMERA", true);
+    native.setCamCoord(camera3, -1000, -1000, 1000);
+    native.setCamRot(camera3, 0, 0, 1, 2);
+    native.setCamFov(camera3, 90);
+
+    native.setCamActive(camera, true);
+    native.renderScriptCams(true, false, 16, true, false, 0);
+
+    alt.on(EventNames.player.localClient.LoginStartCam, () => {
+      alt.setTimeout(async () => {
+        native.setCamActiveWithInterp(camera2, camera, 10000, 1, 1);
+      }, 1000);
+      alt.setTimeout(async () => {
+        native.setCamActiveWithInterp(camera3, camera2, 10000, 1, 1);
+      }, 15000);
+    });
+  }
+  setTimeout(LoginCamera, 10); // Agar in nabashad camera e'mal nemishavad
+  await VGView.emit(
+    WebViewStatus.login.name,
+    EventNames.login.clientWEB.SetDataWeb,
+    {
+      WebLanguage: await LocalStorage.get("WebLanguage"),
+      WebDarkMode: await LocalStorage.get("WebDarkMode"),
+      Remember: await LocalStorage.get("Remember"),
+      LoginData: await LocalStorage.get("LoginData"),
+    }
+  );
+  VGView.on(EventNames.login.WEBclient.LoginAccount, async (obj) => {
+    if (obj.Remember == true) {
+      LocalStorage.set("Remember", true);
+      LocalStorage.set("LoginData", {
+        password: obj.Password,
+        username: obj.Username,
+      });
+    } else {
+      if ((await LocalStorage.get("LoginData")) != undefined) {
+        LocalStorage.set("Remember", false);
+        LocalStorage.set("LoginData", undefined);
+      }
+    }
+    await alt.emitServer("Login_Account_To_Server", {
+      password: obj.Password,
+      username: obj.Username,
+    });
+  });
+
+  alt.onServer("CallBack_Login_Account_To_Server", async (state) => {
+    if (state == true) {
+      await VGView.emit(
+        WebViewStatus.login.name,
+        EventNames.login.clientWEB.LoginSuccessfully
+      );
+
+      if (await VGView.unload(WebViewStatus.login.name)) {
+        native.displayRadar(true);
+        native.destroyAllCams(true);
+        native.renderScriptCams(false, false, 0, false, false, 0);
+        alt.emit(EventNames.allVue.localClient.loadWebviews);
+      }
+    }
+  });
+}
+
+// In OldLoadLoginPage() ghadimi ast va faghat be dalile dashtane asamiye event ha negah dashte shode ast
+// Baad az etmame kar ba event name ha in ghesmat bayad hazf gardad
+async function OldLoadLoginPage() {
   View.Add("Login");
   const view = new webview_tools(
     "http://assets/Webview/client/login/index.html",
--- resources/Gamemode/client/views/viewCreator.js
+++ resources/Gamemode/client/views/viewCreator.js
@@ -2,7 +2,7 @@ import * as alt from "alt-client";
 
 let viewStatus = [];
 export const view = new alt.WebView(
-  "http://assets/Webview/client/allViews/index.html"
+  "http://assets/Webview/client/allViews/test.html"
 );
 export class View {
   static Add(name) {
@@ -21,18 +21,18 @@ export class View {
 let ControllerStatus = false;
 export function PlayerController(status = true) {
   if (status && !ControllerStatus) {
-    alt.toggleGameControls(false);
+    // alt.toggleGameControls(false);
 
-    alt.setCursorPos(new alt.Vector2(0.5, 0.5), true);
-    alt.showCursor(true);
-    view.focus();
+    // alt.setCursorPos(new alt.Vector2(0.5, 0.5), true);
+    // alt.showCursor(true);
+    // view.focus();
     ControllerStatus = true;
   } else if (!status && ControllerStatus) {
     if (viewStatus.indexOf("Clothes") != -1) return; // if Clothes on top
 
-    view.unfocus();
-    alt.showCursor(false);
-    alt.toggleGameControls(true);
+    // view.unfocus();
+    // alt.showCursor(false);
+    // alt.toggleGameControls(true);
     ControllerStatus = false;
   }
 }
