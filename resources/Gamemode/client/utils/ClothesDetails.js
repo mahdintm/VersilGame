dfebed1cca72e3a3fa7b37f98c3202ebef47311c
Toofan <Toofan534@gmail.com>
2022-08-18 05:21:18 +0430

    Clothes has been updated

---
+++ resources/Gamemode/client/utils/ClothesDetails.js
@@ -0,0 +1,43 @@
+import * as alt from "alt";
+export const ClothesDetails = {
+  InteriorsID: [198145, 171265],
+  Suggestions: {
+    1: {
+      male: [
+        { componentID: 3, drawableID: 1, textureID: 0 },
+        { componentID: 4, drawableID: 10, textureID: 0 },
+        { componentID: 6, drawableID: 97, textureID: 0 },
+        { componentID: 8, drawableID: 3, textureID: 0 },
+        { componentID: 11, drawableID: 4, textureID: 0 },
+      ],
+      female: [],
+    },
+  },
+  ClothesPreviewNPCs: {
+    name: "ClothesPreviewNPCs",
+    MaleModelHash: alt.hash("MP_M_Freemode_01"),
+    FemaleModelHash: alt.hash("MP_F_Freemode_01"),
+    isNetwork: false,
+    bScriptHostPed: false,
+    isFreezed: true,
+    Interior: {
+      198145: {
+        pos: { x: 71.288, y: -1387.2, z: 28.374, heading: 175.74803466570498 },
+        camera: {
+          x: 71.42,
+          y: -1389.125,
+          z: 29.364,
+          rx: 0,
+          ry: 0,
+          rz: -0.049473900347948074,
+        },
+      },
+      171265: {
+        x: -819.917,
+        y: -1067.196,
+        z: 10.318,
+        heading: 116.22047718094265,
+      },
+    },
+  },
+};
