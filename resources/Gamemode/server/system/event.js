073d728e581f1c7180685f534673bcfe5187d4c6
Toofan <Toofan534@gmail.com>
2022-08-19 04:47:52 +0430

    event.js ServerSide has been updated

--- resources/Gamemode/server/system/event.js
+++ resources/Gamemode/server/system/event.js
@@ -7,89 +7,93 @@ import { send_email } from "./email";
 import { sms } from "./sms";
 import { StaffPoint } from "./staff";
 import { LoadedVehicels } from "./vehicles";
+import { playerDetails } from "../utils/playerDetails";
+import { EventNames } from "../utils/eventNames";
 
-let ban_ip = "::ffff:192.168.90.100"
+let ban_ip = "::ffff:192.168.90.100";
 
-alt.on('beforePlayerConnect', (player) => {
-    player.setSyncedMeta("hasLogin", false);
-})
+alt.on("beforePlayerConnect", (player) => {
+  player.setSyncedMeta("hasLogin", false);
+});
 
 alt.on("connectionQueueAdd", (info, a, b) => {
-    // console.log(info.discordUserID)
-    if (info.discordUserID == "") return info.decline("Please open the Discord App then TryAgain.")
-    if (!LoadedVehicels) return info.decline("Please Try Again then 1 Minutes.")
-    info.accept();
-
-})
+  // console.log(info.discordUserID)
+  if (info.discordUserID == "")
+    return info.decline("Please open the Discord App then TryAgain.");
+  // if (!LoadedVehicels) return info.decline("Please Try Again then 1 Minutes.")
+  info.accept();
+});
 
 alt.on("playerConnect", async (player) => {
-    player.spawn(-66.84395599365234, -802.20615234375, 44.2255859375);
-    player.model = Object.values(character.skin)[0];
-    player.setMeta("Gender", "male");
-    player.dimension = 999;
-    player.visible = true;
-    player.setSyncedMeta("hasLogin", false);
+  alt.emitClient(player, EventNames.player.server.PlayerDetails, playerDetails);
+
+  player.spawn(-66.84395599365234, -802.20615234375, 44.2255859375);
+  player.model = Object.values(character.skin)[0];
+  player.setMeta("Gender", "male");
+  player.dimension = 999;
+  player.visible = true;
+  player.setSyncedMeta("hasLogin", false);
 });
 alt.onClient("SyncData_LOCAL", (player, data) => {
-    // player.setSyncedMeta("Discordid", data.DiscordID);
-    player.setSyncedMeta("Language", data.Language);
+  // player.setSyncedMeta("Discordid", data.DiscordID);
+  player.setSyncedMeta("Language", data.Language);
 });
 alt.onClient("RequestCodeValidation", async (player, data) => {
-    let codes = {
-        EmailCode: await GenerateCode(6),
-        PhoneCode: await GenerateCode(6),
-    };
-    sms.send(
-        data.PhoneNumber,
-        `سلام ${data.name.first + " " + data.name.last} عزیز
+  let codes = {
+    EmailCode: await GenerateCode(6),
+    PhoneCode: await GenerateCode(6),
+  };
+  sms.send(
+    data.PhoneNumber,
+    `سلام ${data.name.first + " " + data.name.last} عزیز
     کد تایید عضویت شما در مجموعه ورسیل گیم ${codes.PhoneCode} است.
     به جمع ورسیلی ها خوش آمدید!`
-    );
-    send_email.VerifyCode({
-        firstname: data.name.first,
-        lastname: data.name.last,
-        email: data.Email,
-        code: codes.EmailCode,
-    });
-    player.setMeta("ValidateCodes", codes);
-    alt.emitClient(player, "CallBackRequestCodeValidation");
+  );
+  send_email.VerifyCode({
+    firstname: data.name.first,
+    lastname: data.name.last,
+    email: data.Email,
+    code: codes.EmailCode,
+  });
+  player.setMeta("ValidateCodes", codes);
+  alt.emitClient(player, "CallBackRequestCodeValidation");
 });
 alt.onClient("RequestCodeValidationEntered", (player, data) => {
-    console.log("Send " + data);
-    let playerdata = player.getMeta("ValidateCodes");
-    if (
-        data.PhoneCode == playerdata.PhoneCode &&
-        data.EmailCode == playerdata.EmailCode
-    ) {
-        return alt.emitClient(
-            player,
-            "CallBackRequestCodeValidationEntered",
-            true,
-            "WiaraServerIsBestServer"
-        );
-    }
+  console.log("Send " + data);
+  let playerdata = player.getMeta("ValidateCodes");
+  if (
+    data.PhoneCode == playerdata.PhoneCode &&
+    data.EmailCode == playerdata.EmailCode
+  ) {
     return alt.emitClient(
-        player,
-        "CallBackRequestCodeValidationEntered",
-        false,
-        ""
+      player,
+      "CallBackRequestCodeValidationEntered",
+      true,
+      "WiaraServerIsBestServer"
     );
+  }
+  return alt.emitClient(
+    player,
+    "CallBackRequestCodeValidationEntered",
+    false,
+    ""
+  );
 });
 await alt.on("playerDisconnect", async (player, reason) => {
-    if (!player.getSyncedMeta("hasLogin")) return;
-    DiscordHook.newhook.disconnect({
-        sqlid: await PlayerData.get(player, "pId"),
-        username: await PlayerData.get(player, "pName"),
-    });
-    StaffPoint.sarOFF(player)
-    PlayerData.delete(player);
-    playerIdGame.delete(player);
+  if (!player.getSyncedMeta("hasLogin")) return;
+  DiscordHook.newhook.disconnect({
+    sqlid: await PlayerData.get(player, "pId"),
+    username: await PlayerData.get(player, "pName"),
+  });
+  StaffPoint.sarOFF(player);
+  PlayerData.delete(player);
+  playerIdGame.delete(player);
 });
 await alt.onClient("Login_Account_To_Server", PlayerData.login);
 await alt.onClient("Register_Account_To_Server", PlayerData.register);
 
 setTimeout(() => {
-    console.log(` 
+  console.log(` 
     ............         .............
     ........                  ........
     .....                        .....
