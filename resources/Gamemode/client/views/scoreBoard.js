42ad0ad1a00481d2d73775ab94aba43f74a3e894
Toofan <Toofan534@gmail.com>
2022-08-22 02:09:44 +0430

    ScoreBoard has been updated

--- resources/Gamemode/client/views/scoreBoard.js
+++ resources/Gamemode/client/views/scoreBoard.js
@@ -1,15 +1,23 @@
 /// <reference types="@altv/types-client" />
 /// <reference types="@altv/types-natives" />
 import * as alt from "alt-client";
+import { ChangeValueFromVariable } from "../system/everyTick";
 
 import { EventNames } from "../utils/eventNames";
 import { WebViewStatus } from "../utils/WebViewStatus";
 import { VGView } from "./webViewController";
+
+export function GetScoreBoardDetails() {
+  return alt.emitServer(EventNames.scoreBoard.client.GetScoreBoardDetails);
+}
 alt.on(EventNames.allVue.localClient.loadWebviews, () => {
   alt.on(EventNames.scoreBoard.localClient.ActiveScoreBoard, async (Status) => {
     if (Status) {
-      return alt.emitServer(EventNames.scoreBoard.client.GetScoreBoardDetails);
+      await VGView.load(WebViewStatus.scoreBoard.name);
+      alt.emitServer(EventNames.scoreBoard.client.GetScoreBoardDetails);
+      return ChangeValueFromVariable("ScoreBoardStatus", true);
     }
+    ChangeValueFromVariable("ScoreBoardStatus", false);
     if (!(await VGView.close(WebViewStatus.scoreBoard.name))) {
       await VGView.emit(
         WebViewStatus.scoreBoard.name,
@@ -23,8 +31,6 @@ alt.on(EventNames.allVue.localClient.loadWebviews, () => {
   alt.onServer(
     EventNames.scoreBoard.server.SetScoreBoardDetails,
     async (ScoreBoardDetails) => {
-      await VGView.load(WebViewStatus.scoreBoard.name);
-
       await VGView.emit(
         WebViewStatus.scoreBoard.name,
         EventNames.scoreBoard.clientWEB.SetScoreBoardDetails,
--- resources/Gamemode/server/system/ScoreBoard.js
+++ resources/Gamemode/server/system/ScoreBoard.js
@@ -1,31 +1,43 @@
 /// <reference types="@altv/types-server" />
 import * as alt from "alt-server";
 import { EventNames } from "../utils/eventNames";
-const ScoreBoardDetails = {
-  PlayersLimit: 1024,
-  ConnectedPlayers: 2,
-  UpTimeServer: "01H 20M",
-  PlayerPlayTime: "00H 30M",
-  PlayersDetails: [
-    {
-      ID: 1,
-      Name: "Toofan",
-      Ping: 18,
-      Level: 20,
-    },
-    {
-      ID: 2,
-      Name: "Mahdi",
-      Ping: 3,
-      Level: 20,
-    },
-  ],
-};
+import { PlayerData } from "./account";
 
-alt.onClient(EventNames.scoreBoard.client.GetScoreBoardDetails, (player) => {
-  alt.emitClient(
-    player,
-    EventNames.scoreBoard.server.SetScoreBoardDetails,
-    ScoreBoardDetails
-  );
-});
+class VGScoreBoardServer {
+  static async #GetPlayersDetails() {
+    let PlayersDetails = [];
+    for await (const player of alt.Player.all) {
+      PlayersDetails.push({
+        ID: await PlayerData.get(player, "gameID"),
+        Name: await PlayerData.get(player, "pName"),
+        Ping: player.ping,
+        Level: await PlayerData.get(player, "pLevel"),
+      });
+    }
+    return PlayersDetails;
+  }
+  static #GetConnectedPlayers() {
+    return alt.Player.all.length;
+  }
+  static async GetScoreBoardDetails() {
+    const ScoreBoardDetails = {
+      PlayersLimit: 1024,
+      ConnectedPlayers: this.#GetConnectedPlayers(),
+      UpTimeServer: "unknow",
+      PlayerPlayTime: "unknow",
+      PlayersDetails: await this.#GetPlayersDetails(),
+    };
+    return ScoreBoardDetails;
+  }
+}
+
+alt.onClient(
+  EventNames.scoreBoard.client.GetScoreBoardDetails,
+  async (player) => {
+    alt.emitClient(
+      player,
+      EventNames.scoreBoard.server.SetScoreBoardDetails,
+      await VGScoreBoardServer.GetScoreBoardDetails()
+    );
+  }
+);
