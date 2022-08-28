2491896d44410874e7812ba79d0afce4bb24d8dd
Mahdi Nemati <nematimahdi88@gmail.com>
2022-08-28 05:48:05 +0430

    Dialog system has been updated.

--- resources/Gamemode/server/utils/dialogs.js
+++ resources/Gamemode/server/utils/dialogs.js
@@ -1,16 +1,12 @@
 import fs from 'fs'
 export class Language {
-    static async GetValue(LanguageType, Key) {
-        // try {
-        // console.log(LanguageType)
-        // return fs.readFile(`./resources/Gamemode/server/utils/locale/${LanguageType}.json`, 'utf8', (err, data) => {
-        //     if (err) {
-        //         console.error(err);
-        //         return;
-        //     }
-        //     return JSON.parse(data)[Key];
-        // });
-
-        return await JSON.parse(await fs.promises.readFile(`./resources/Gamemode/server/utils/locale/${LanguageType}.json`, 'utf8'))[Key]
+    static async GetValue(LanguageType, Key, args) {
+        let text = await JSON.parse(await fs.promises.readFile(`./resources/Gamemode/server/utils/locale/${LanguageType}.json`, 'utf8'))[Key]
+        let i = 0
+        for await (const args_ of args) {
+            text = await text.replace(`{${i}}`, args_)
+            i++
+        }
+        return text
     }
 }
