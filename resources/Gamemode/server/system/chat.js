54498739ad0317ee2229850cbc37ea62ce6fa7ab
Mahdi Nemati <nematimahdi88@gmail.com>
2022-08-30 14:58:09 +0430

    Mute chat va Mute CMD has been created.

--- resources/Gamemode/server/system/chat.js
+++ resources/Gamemode/server/system/chat.js
@@ -3,38 +3,37 @@ import * as alt from "alt-server";
 import { findbadword } from "../utils/badword_detect";
 import { EventNames } from "../utils/eventNames";
 import { PlayerData } from "./account";
-// import { findbadword } from "../utility/badword_detect";
 let cmdHandlers = {};
-// let mutedPlayers = new Map();
 
-// function checkmute(player) {
-//     if (GetPlayerData(player, "pMute")) {
-//         if (GetPlayerData(player, "pMute_Time") <= Date.now) {
-//             return true;
-//         } else {
-//             return false;
-//         }
-//     } else {
-//         return true
-//     }
-// }
-
-/**
- * For send a message in chat system to a player
- * @param {object} player player object altv
- * @param {string} msg Your message
- *
- */
+export async function CheckMute_Chat(player) {
+  if (await PlayerData.get(player, 'pMute_Chat')) {
+    if (await PlayerData.get(player, "pMute_Time_Chat") <= Date.now) {
+      await PlayerData.set(player, 'pMute_Chat', 0, true)
+      await PlayerData.set(player, 'pMute_Time_Chat', 0, true)
+      return true
+    } else {
+      return false
+    }
+  } else {
+    return true
+  }
+}
+export async function CheckMute_CMD(player) {
+  if (await PlayerData.get(player, 'pMute_CMD')) {
+    if (await PlayerData.get(player, "pMute_Time_CMD") <= Date.now) {
+      await PlayerData.set(player, 'pMute_CMD', 0, true)
+      await PlayerData.set(player, 'pMute_Time_CMD', 0, true)
+      return true
+    } else {
+      return false
+    }
+  } else {
+    return true
+  }
+}
 export async function sendchat(player, msg) {
   await alt.emitClient(player, EventNames.chat.server.Message, Date.now(), null, msg);
 }
-
-/**
- * For Create CMD
- * @param {string} cmd Name of Command
- * @param {Function} callback function of Command
- *
- */
 export function registerCmd(cmd, callback) {
   cmd = cmd.toLowerCase();
   if (cmdHandlers[cmd] !== undefined) {
@@ -43,7 +42,6 @@ export function registerCmd(cmd, callback) {
     cmdHandlers[cmd] = callback;
   }
 }
-
 function invokeCmd(player, cmd, args) {
   cmd = cmd.toLowerCase();
   const callback = cmdHandlers[cmd];
@@ -54,56 +52,36 @@ function invokeCmd(player, cmd, args) {
   }
 }
 alt.onClient(EventNames.chat.client.Loaded, (player) => {
-  alt.emitClient(
-    player,
-    EventNames.chat.server.Message,
-    Date.now(),
-    null,
-    "Connected To Server!"
-  );
+  alt.emitClient(player, EventNames.chat.server.Message, Date.now(), null, "Connected To Server!");
 });
 alt.onClient(EventNames.chat.client.Message, async (player, msg) => {
   if (msg[0] === "/") {
-    msg = msg.trim().slice(1);
-    if (msg.length > 0) {
-      let args = msg.split(" ");
-      let cmd = args.shift();
-      invokeCmd(player, cmd, args);
+    if (await CheckMute_CMD(player)) {
+      msg = msg.trim().slice(1);
+      if (msg.length > 0) {
+        let args = msg.split(" ");
+        let cmd = args.shift();
+        invokeCmd(player, cmd, args);
+      }
+    } else {
+      sendchat(player, "You are muted.")
     }
   } else {
-    // if (checkmute(player)) {
-    msg = msg.trim();
-    if (!msg.length) return;
-    let PlayerName = await PlayerData.get(player, "pName");
-    let msgfilltered = findbadword(msg)
-      .replace(/</g, "&lt;")
-      .replace(/'/g, "&#39")
-      .replace(/"/g, "&#34");
-    const playerPos = player.pos;
-    const players = alt.Player.all;
-    for (let p of players) {
-      if (
-        playerPos.distanceTo(p.pos) > process.env.Chat_Distance ||
-        player == p
-      )
-        continue;
-      alt.emitClient(
-        p,
-        EventNames.chat.server.Message,
-        Date.now(),
-        PlayerName,
-        msgfilltered
-      );
+    if (await CheckMute_Chat(player)) {
+      msg = msg.trim();
+      if (!msg.length) return;
+      let PlayerName = await PlayerData.get(player, "pName");
+      let msgfilltered = findbadword(msg).replace(/</g, "&lt;").replace(/'/g, "&#39").replace(/"/g, "&#34");
+      const playerPos = player.pos;
+      const players = alt.Player.all;
+      for await (let player_ of players) {
+        if (playerPos.distanceTo(player_.pos) > await ServerSetting.get("Chat_Distance") || player == player_) continue;
+        alt.emitClient(player_, EventNames.chat.server.Message, Date.now(), PlayerName, msgfilltered);
+      }
+      alt.emitClient(player, EventNames.chat.server.Message, Date.now(), PlayerName, msgfilltered);
+    } else {
+      sendchat(player, "You are muted.")
     }
-    alt.emitClient(
-      player,
-      EventNames.chat.server.Message,
-      Date.now(),
-      PlayerName,
-      msgfilltered
-    );
-    // } else {
-    //     alt.emitClient(player, "chat:message", null, "You are muted.");
-    // }
   }
 });
+
