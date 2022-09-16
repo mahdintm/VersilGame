9484cf81107fbc25018d4c59a8a60fe67b231f36
Mahdi Nemati <nematimahdi88@gmail.com>
2022-09-16 04:02:23 +0000

    All Ipls Has been Added

---
+++ resources/Gamemode/server/utils/ipllocations.js
@@ -0,0 +1,247 @@
+export const ipl_location = {
+    // ■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■▶The Diamond Casino and Resort◀■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+    [1]: { name: "Casino Penthouse", x: 976.6364, y: 70.29476, z: 115.1641, ipl: "vw_casino_penthouse" },
+
+    [2]: { name: "Casino Windows", x: 976.6364, y: 70.29476, z: 115.1641, ipl: "hei_dlc_windows_casino" },
+
+    [3]: { name: "Casino Parkhouse", x: 1380.0000, y: 200.0000, z: -50.0000, ipl: "vw_casino_carpark" },
+
+    [4]: { name: "Casino Garage", x: 1295.0000, y: 230.0000, z: -50.0000, ipl: "vw_casino_garage" },
+
+    [5]: { name: "Casino Main", x: 1100.0000, y: 220.0000, z: -50.0000, ipl: "vw_casino_main" },
+
+    // ■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■▶ Online Bunkers ◀■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+
+    [6]: { name: "ZancudoBunker", x: -3058.714, y: 3329.19, z: 12.5844, ipl: "gr_case10_bunkerclosed" },
+
+    [7]: { name: "Route68Bunker", x: 24.43542, y: 2959.705, z: 58.35517, ipl: "gr_case9_bunkerclosed" },
+
+    [8]: { name: "OilfieldsBunker", x: 481.0465, y: 2995.135, z: 43.96672, ipl: "gr_case3_bunkerclosed" },
+
+    [9]: { name: "DesertBunker", x: 848.6175, y: 2996.567, z: 45.81612, ipl: "	gr_case0_bunkerclosed" },
+
+    [10]: { name: "SmokeTreeBunker", x: 2126.785, y: 3335.04, z: 48.21422, ipl: "gr_case1_bunkerclosed" },
+
+    [11]: { name: "ScrapyardBunker", x: 848.6175, y: 2996.567, z: 45.81612, ipl: "gr_case2_bunkerclosed" },
+
+    [12]: { name: "GrapeseedBunker", x: 2493.654, y: 3140.399, z: 51.28789, ipl: "gr_case0_bunkerclosed" },
+
+    [13]: { name: "PalletoBunker", x: -783.0755, y: 5934.686, z: 24.31475, ipl: "	gr_case7_bunkerclosed" },
+
+    [14]: { name: "Route1Bunker", x: -3180.466, y: 1374.192, z: 19.9597, ipl: "gr_case11_bunkerclosed" },
+
+    [15]: { name: "FarmhouseBunker", x: 1570.372, y: 2254.549, z: 78.89397, ipl: "	gr_case6_bunkerclosed" },
+
+    [17]: { name: "RatonCanyonBunker", x: -391.3216, y: 4363.728, z: 58.65862, ipl: "gr_case4_bunkerclosed" },
+
+    // ■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■▶ Online Apartments ◀■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+
+    [18]: { name: "Modern 1 Apartment", x: -786.8663, y: 315.7642, z: 217.6385, ipl: "apa_v_mp_h_01_a" },
+
+    [19]: { name: "Modern 2 Apartment", x: -784.971435546875, y: 323.5780334472656, z: 211.982421875, ipl: "apa_v_mp_h_01_c" },
+
+    [20]: { name: "Modern 3 Apartment", x: -774.0126, y: 342.0428, z: 196.6864, ipl: "apa_v_mp_h_02_a" },
+
+    [21]: { name: "Mody 2 Apartment", x: -786.8195, y: 315.5634, z: 187.9137, ipl: "apa_v_mp_h_02_c" },
+
+    [22]: { name: "Mody 3 Apartment", x: -774.1382, y: 342.0316, z: 196.6864, ipl: "apa_v_mp_h_02_b	" },
+
+    [23]: { name: "Vibrant 1 Apartment", x: -786.6245, y: 315.6175, z: 217.6385, ipl: "apa_v_mp_h_03_a" },
+
+    [24]: { name: "Vibrant 2 Apartment", x: -786.9584, y: 315.7974, z: 187.9135, ipl: "apa_v_mp_h_03_c" },
+
+    [25]: { name: "Vibrant 3 Apartment", x: -774.0223, y: 342.1718, z: 196.6863, ipl: "apa_v_mp_h_03_b" },
+
+    [26]: { name: "Sharp 1 Apartment", x: -787.0902, y: 315.7039, z: 217.6384, ipl: "apa_v_mp_h_04_a" },
+
+    [27]: { name: "Sharp 2 Apartment", x: -787.0155, y: 315.7071, z: 187.9135, ipl: "apa_v_mp_h_04_c" },
+
+    [28]: { name: "Sharp 3 Apartment", x: -773.8976, y: 342.1525, z: 196.6863, ipl: "apa_v_mp_h_04_b" },
+
+    [29]: { name: "Monochrome 1 Apartment", x: -786.9887, y: 315.7393, z: 217.6386, ipl: "apa_v_mp_h_05_a" },
+
+    [30]: { name: "Monochrome 2 Apartment", x: -786.8809, y: 315.6634, z: 187.9136, ipl: "apa_v_mp_h_05_c" },
+
+    [31]: { name: "Monochrome 3 Apartment", x: -774.0675, y: 342.0773, z: 196.6864, ipl: "apa_v_mp_h_05_b" },
+
+    [32]: { name: "Seductive 1 Apartment", x: -787.1423, y: 315.6943, z: 217.6384, ipl: "apa_v_mp_h_06_a" },
+
+    [33]: { name: "Seductive 2 Apartment", x: -787.0961, y: 315.815, z: 187.9135, ipl: "apa_v_mp_h_06_c" },
+
+    [34]: { name: "Seductive 3 Apartment", x: -773.9552, y: 341.9892, z: 196.6862, ipl: "apa_v_mp_h_06_b" },
+
+    [35]: { name: "Regal 1 Apartment", x: -787.029, y: 315.7113, z: 217.6385, ipl: "apa_v_mp_h_07_a" },
+
+    [36]: { name: "Regal 2 Apartment", x: -787.0574, y: 315.6567, z: 187.9135, ipl: "apa_v_mp_h_07_c" },
+
+    [37]: { name: "Regal 3 Apartment", x: -774.0109, y: 342.0965, z: 196.6863, ipl: "apa_v_mp_h_07_b" },
+
+    [38]: { name: "Aqua 1 Apartment", x: -786.9469, y: 315.5655, z: 217.6383, ipl: "apa_v_mp_h_08_a" },
+
+    [39]: { name: "Aqua 2 Apartment", x: -786.9756, y: 315.723, z: 187.9134, ipl: "apa_v_mp_h_08_c" },
+
+    [40]: { name: "Aqua 3 Apartment", x: -774.0349, y: 342.0296, z: 196.6862, ipl: "apa_v_mp_h_08_b" },
+
+
+    // ■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■▶ Arcadius Business Centre ◀■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+
+    [41]: { name: "Executive Rich", x: -141.1987, y: -620.913, z: 168.8205, ipl: "ex_dt1_02_office_02b" },
+
+    [42]: { name: "Executive Cool", x: -141.5429, y: -620.9524, z: 168.8204, ipl: "ex_dt1_02_office_02c" },
+
+    [43]: { name: "Executive Contrast", x: -141.2896, y: -620.9618, z: 168.8204, ipl: "ex_dt1_02_office_02a" },
+
+    [44]: { name: "Old Spice Warm", x: -141.4966, y: -620.8292, z: 168.8204, ipl: "ex_dt1_02_office_01a" },
+
+    [45]: { name: "Old Spice Classical", x: -141.3997, y: -620.9006, z: 168.8204, ipl: "ex_dt1_02_office_01b" },
+
+    [46]: { name: "Old Spice Vintage", x: -141.5361, y: -620.9186, z: 168.8204, ipl: "ex_dt1_02_office_01c" },
+
+    [47]: { name: "Power Broker Ice", x: -141.392, y: -621.0451, z: 168.8204, ipl: "ex_dt1_02_office_03a" },
+
+    [48]: { name: "Power Broker Conservative", x: -141.1945, y: -620.8729, z: 168.8204, ipl: "ex_dt1_02_office_03b" },
+
+    [49]: { name: "Power Broker Polished", x: -141.4924, y: -621.0035, z: 168.8205, ipl: "	ex_dt1_02_office_03c" },
+
+    // ■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■▶ Maze Bank Building ◀■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+
+    [50]: { name: "Executive Rich", x: -75.8466, y: -826.9893, z: 243.3859, ipl: "ex_dt1_11_office_02b" },
+
+    [51]: { name: "Executive Cool", x: -75.49945, y: -827.05, z: 243.386, ipl: "ex_dt1_11_office_02c" },
+
+    [52]: { name: "Executive Contrast", x: -75.49827, y: -827.1889, z: 243.386, ipl: "	ex_dt1_11_office_02a" },
+
+    [53]: { name: "Old Spice Warm", x: -75.44054, y: -827.1487, z: 243.3859, ipl: "ex_dt1_11_office_01a" },
+
+    [54]: { name: "Old Spice Classical", x: -75.63942, y: -827.1022, z: 243.3859, ipl: "ex_dt1_11_office_01b" },
+
+    [55]: { name: "Old Spice Vintage", x: -75.47446, y: -827.2621, z: 243.386, ipl: "ex_dt1_11_office_01c" },
+
+    [56]: { name: "Power Broker Ice", x: -75.56978, y: -827.1152, z: 243.3859, ipl: "ex_dt1_11_office_03a" },
+
+    [57]: { name: "Power Broker Conservative", x: -75.51953, y: -827.0786, z: 243.3859, ipl: "ex_dt1_11_office_03b" },
+
+    [58]: { name: "Power Broker Polished", x: -75.41915, y: -827.1118, z: 243.3858, ipl: "ex_dt1_11_office_03c" },
+
+    // ■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■▶ Lom Bank ◀■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+
+    [59]: { name: "Executive Rich", x: -1579.756, y: -565.0661, z: 108.523, ipl: "ex_sm_13_office_02b" },
+
+    [60]: { name: "Executive Cool", x: -1579.678, y: -565.0034, z: 108.5229, ipl: "ex_sm_13_office_02c" },
+
+    [61]: { name: "Executive Contrast", x: -1579.583, y: -565.0399, z: 108.5229, ipl: "ex_sm_13_office_02a" },
+
+    [62]: { name: "Old Spice Warm", x: -1579.702, y: -565.0366, z: 108.5229, ipl: "ex_sm_13_office_01a" },
+
+    [63]: { name: "Old Spice Classical", x: -1579.643, y: -564.9685, z: 108.5229, ipl: "ex_sm_13_office_01b" },
+
+    [64]: { name: "Old Spice Vintage", x: -1579.681, y: -565.0003, z: 108.523, ipl: "ex_sm_13_office_01c" },
+
+    [65]: { name: "Power Broker Ice", x: -1579.677, y: -565.0689, z: 108.5229, ipl: "ex_sm_13_office_03a" },
+
+    [66]: { name: "Power Broker Conservative", x: -1579.708, y: -564.9634, z: 108.5229, ipl: "ex_sm_13_office_03b" },
+
+    [67]: { name: "Power Broker Polished", x: -1579.693, y: -564.8981, z: 108.5229, ipl: "ex_sm_13_office_03c" },
+
+    // ■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■▶ Maze Bank West ◀■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+
+    [68]: { name: "Executive Rich", x: -1392.667, y: -480.4736, z: 72.04217, ipl: "ex_sm_15_office_02b" },
+
+    [69]: { name: "Executive Cool", x: -1392.542, y: -480.4011, z: 72.04211, ipl: "ex_sm_15_office_02c" },
+
+    [70]: { name: "Executive Contrast", x: -1392.626, y: -480.4856, z: 72.04212, ipl: "ex_sm_15_office_02a" },
+
+    [71]: { name: "Old Spice Warm", x: -1392.617, y: -480.6363, z: 72.04208, ipl: "ex_sm_15_office_01a" },
+
+    [72]: { name: "Old Spice Classical", x: -1392.532, y: -480.7649, z: 72.04207, ipl: "ex_sm_15_office_01b" },
+
+    [73]: { name: "Old Spice Vintage", x: -1392.611, y: -480.5562, z: 72.04214, ipl: "ex_sm_15_office_01c" },
+
+    [74]: { name: "Power Broker Ice", x: -1392.563, y: -480.549, z: 72.0421, ipl: "ex_sm_15_office_03a" },
+
+    [75]: { name: "Power Broker Conservative", x: -1392.528, y: -480.475, z: 72.04206, ipl: "ex_sm_15_office_03b" },
+
+    [76]: { name: "Power Broker Polished", x: -1392.416, y: -480.7485, z: 72.04207, ipl: "	ex_sm_15_office_03c" },
+
+    // ■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■▶ Clubhouse & Warehouses ◀■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+
+    [77]: { name: "Clubhouse 1", x: 1107.04, y: -3157.399, z: -37.51859, ipl: "bkr_biker_interior_placement_interior_0_biker_dlc_int_01_milo" },
+
+    [78]: { name: "Clubhouse 2", x: 998.4809, y: -3164.711, z: -38.90733, ipl: "bkr_biker_interior_placement_interior_1_biker_dlc_int_02_milo" },
+
+    [79]: { name: "Warehouse 1", x: 1009.5, y: -3196.6, z: -38.99682, ipl: "bkr_biker_interior_placement_interior_2_biker_dlc_int_ware01_milo" },
+
+    [80]: { name: "Warehouse 2", x: 1051.491, y: -3196.536, z: -39.14842, ipl: "bkr_biker_interior_placement_interior_3_biker_dlc_int_ware02_milo" },
+
+    [81]: { name: "Warehouse 3", x: 1093.6, y: -3196.6, z: -38.99841, ipl: "bkr_biker_interior_placement_interior_4_biker_dlc_int_ware03_milo" },
+
+    [82]: { name: "Warehouse 4", x: 1121.897, y: -3195.338, y: -40.4025, ipl: "bkr_biker_interior_placement_interior_5_biker_dlc_int_ware04_milo" },
+
+    [83]: { name: "Warehouse 5", x: 1165, y: -3196.6, z: -39.01306, ipl: "	bkr_biker_interior_placement_interior_6_biker_dlc_int_ware05_milo" },
+
+    [84]: { name: "Warehouse Small", x: 1094.988, y: -3101.776, z: -39.00363, ipl: "ex_exec_warehouse_placement_interior_1_int_warehouse_s_dlc_milo" },
+
+    [85]: { name: "Warehouse Medium", x: 1056.486, y: -3105.724, z: -39.00439, ipl: "ex_exec_warehouse_placement_interior_0_int_warehouse_m_dlc_milo" },
+
+    [86]: { name: "Warehouse Large", x: 1006.967, y: -3102.079, z: -39.0035, ipl: "	ex_exec_warehouse_placement_interior_2_int_warehouse_l_dlc_milo" },
+
+    [87]: { name: "Cargarage", x: 994.5925, y: -3002.594, z: -39.64699, ipl: "imp_impexp_interior_placement_interior_1_impexp_intwaremed_milo_" },
+
+    // ■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■▶ Special Locations ◀■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+
+    [88]: { name: "Normal Cargo Ship", x: -163.3628, y: -2385.161, z: 5.999994, ipl: "cargoship" },
+
+    [89]: { name: "Sunken Cargo Ship", x: -163.3628, y: -2385.161, z: 5.999994, ipl: "sunkcargoship" },
+
+    [90]: { name: "Burning Cargo Ship", x: -163.3628, y: -2385.161, z: 5.999994, ipl: "SUNK_SHIP_FIRE" },
+
+    [91]: { name: "Red Carpet", x: 300.5927, y: 300.5927, z: 104.3776, ipl: "redCarpet" },
+
+    [92]: { name: "Rekt Stilthouse Destroyed", x: -1020.518, y: 663.27, z: 153.5167, ipl: "DES_StiltHouse_imapend" },
+
+    [93]: { name: "Rekt Stilthouse Rebuild", x: -1020.518, y: 663.27, z: 153.5167, ipl: "DES_stilthouse_rebuild" },
+
+    [94]: { name: "Union Depository", x: 2.6968, y: -667.0166, z: 16.13061, ipl: "FINBANK" },
+
+    [95]: { name: "Trevors Trailer Dirty", x: 1975.552, y: 3820.538, z: 33.44833, ipl: "TrevorsMP" },
+
+    [96]: { name: "Trevors Trailer Clean", x: 1975.552, y: 3820.538, z: 33.44833, ipl: "TrevorsTrailerTidy" },
+
+    [97]: { name: "Stadium", x: -248.6731, y: -2010.603, z: 30.14562, ipl: "SP1_10_real_interior" },
+
+    [98]: { name: "Max Renda Shop", x: -585.8247, y: -282.72, z: 35.45475, ipl: "refit_unload" },
+
+    [99]: { name: "Jewel Store", x: -630.07, y: -236.332, z: 38.05704, ipl: "post_hiest_unload" },
+
+    [100]: { name: "FIB Lobby", x: 110.4, y: -744.2, z: 45.7496, ipl: "FIBlobby" },
+
+    // ■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■▶ GTA World ◀■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+
+    [101]: { name: "Carwash", x: 55.7, y: -1391.3, z: 30.5, ipl: "Carwash_with_spinners" },
+
+    [102]: { name: "Stadium", x: -248.491, y: -2010.509, z: 34.574, ipl: "sp1_10_real_interior_lod" },
+
+    [103]: { name: "Garage in La Mesa", x: 970.27453, y: -1826.56982, z: 31.11477, ipl: "bkr_bi_id1_23_door" },
+
+    [104]: { name: "Red Hill Valley church - Grave", x: -282.4638, y: 2835.84, z: 55.914, ipl: "lr_cs6_08_grave_closed" },
+
+    [105]: { name: "Lost's trailer park", x: 49.49379, y: 3744.472, z: 46.38629, ipl: "methtrailer_grp1" },
+
+    [106]: { name: "Lost Clubhouse", x: 984.1552, y: -95.3662, z: 74.50, ipl: "bkr_bi_hw1_13_int" },
+
+    [107]: { name: "Pillbox hospital", x: 307.1680, y: -590.807, z: 43.280, ipl: "rc12b_default" },
+
+    [108]: { name: "PDM (Simons Car Dealer)", x: -54.30, y: -1109.3767, z: 26.4358, ipl: "shr_int" },
+
+    [109]: { name: "Remove Zancudoe Gates", x: -1599.95, y: 2807.05, z: 17.204, ipl: "CS3_07_MPGates" },
+
+    [110]: { name: "Ferris Wheel", x: -1645.55, y: -1113.04, z: 12.65, ipl: "ferris_finale_Anim" },
+
+    [111]: { name: "Casino Penthouse Glassfront", x: 968.156, y: 0.3060, z: 111.2922, ipl: "hei_dlc_windows_casino" },
+
+    [112]: { name: "Golfflags", x: -1032, y: -84, z: 44, ipl: "golfflags" },
+
+    [113]: { name: "Racetrack", x: 1978, y: 3111, z: 46, ipl: "racetrack01" },
+}
+
+export default { ipl_location }
